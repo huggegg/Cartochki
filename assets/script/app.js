fc1c1b110f586b879a2ee56bda91aa539bf4d4ce
huggegg <kulevtimur123@gmail.com>
2024-02-14 23:47:54 +0300

    0.5 version

--- assets/script/app.js
+++ assets/script/app.js
@@ -17,8 +17,54 @@ const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
 // console.log(inputElement.value)
+createBtn.onclick = function () {
+    if (inputElement.value.length == 0) {
+        return
+    }
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(newNote))
 
-const notes = ['записать блок про массивы', 'сходить за водой']
+    inputElement.value = ''
+}
+
+function getNoteTemplate(note) {
+    return `
+        <div class="flex my-5 justify-center ">
+        <li class="w-[907px] py-14 pl-10 pr-80 bg-[#D9D9D9] rounded-2xl ${note.completed ? 'line-through' : ''}  placeholder:text-[#333] placeholder:font-bold transition-all border">${note.title}</li>
+        <button class="py-12 px-10 bg-[#C3A28A] rounded-2xl relative right-5 ${note.completed ? 'warning' : 'succes'} ">Deleate</button>
+        <button class="relative -top-10 right-0"><img src="assets/svg/ic_outline-edit.svg" alt=""></button>
+        </div>
+    `
+}
+
+
+// 
+// const person = {
+//     firstName: 'Timur',
+//     lastName: 'Kulev',
+//     year: 2006,
+//     languages: ['ru', 'en', 'ch'],
+//     getFullName: function() {
+//         console.log(person.firstName,person.lastName)
+//         return
+//     }
+// }
+// // console.log(typeof person)
+
+// console.log(person.getFullName())
+
+const notes = [{
+    title: 'сходить за водой',
+    completed: false,
+} ,
+{
+        title: 'пересказ',
+        completed: true,
+}
+]
 
 function render() {
 
@@ -31,32 +77,7 @@ function render() {
     for (let note of notes) {
         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
     }
-    
+
 }
 
 render()
-
-createBtn.onclick = function () {
-    if (inputElement.value.length == 0) {
-        return
-    }
-
-        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(inputElement.value))
-
-    inputElement.value = ''
-}
-
-function getNoteTemplate(title) {
-    return `
-        <div class="flex my-5 justify-center ">
-        <li  class="w-[907px] py-14 pl-10 pr-80 bg-[#D9D9D9] rounded-2xl placeholder:text-[#333] placeholder:font-bold transition-all border">${title}</li>
-        <button class="py-12 px-10 bg-[#C3A28A] rounded-2xl relative right-5">Deleate</button>
-        <button class="relative -top-10 right-0"><img src="assets/svg/ic_outline-edit.svg" alt=""></button>
-        </div>
-    `
-}
-
-
-// 
-const person = {}
-console.log(typeof person)
